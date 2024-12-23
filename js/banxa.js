181297bf0b29caaec86fd27725371fcf3c897e57
nextterminal <nextterminal@tutamail.com>
2024-12-23 23:30:09 +0100

    Delete js/banxa.js

--- js/banxa.js
+++
@@ -1,6 +0,0 @@
-!function(callback){
-var b=document.createElement('script'); b.type="text/javascript", b.async=!0,
-b.src="https://sdk.banxa.com/js/banxa-sdk-latest.js";
-var x=document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(b,x);
-if(callback){b.addEventListener("load",function(){callback()})}}
-(yourOnLoadFunction);
